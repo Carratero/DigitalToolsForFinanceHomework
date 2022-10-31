7ab6124963ac8bdda78b1c0fd0ceb91bd327129c
Antonin Carratero <antonincarratero@MacBook-Pro-de-Antonin.local>
2022-10-31 14:00:12 +0100

    create datastruture

---
+++ homework/week5/src/connect.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+# connect.py
+import os
+import sys
+
+# point to the DB
+DATAPATH = '/Users/antonincarratero/git/DigitalToolsForFinanceHomework/homework/week5/data'
+
+sys.stdout.write(DATAPATH)
+
+
